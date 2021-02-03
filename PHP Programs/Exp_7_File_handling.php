add55dc96e21ff11e6ae25bcc168feee7af0b437
rathnachalla <68558385+rathna30@users.noreply.github.com>
2021-02-03 21:23:40 +0530

    Create Exp_7_File_handling.php

---
+++ PHP Programs/Exp_7_File_handling.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+
+$myfile = fopen("newfile.txt","r") or die("Unable to open file");
+$txt = fread($myfile, filesize("newfile.txt"));
+fclose($myfile);
+
+$myfile = fopen("newfile2.txt","w") or die("Unable to open file!");
+fwrite($myfile, $txt);
+echo "successfully read from newfile and written into newfile2 !!";
+fclose($myfile);
+?>
+
+</body>
+</html>
